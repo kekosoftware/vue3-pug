1ed17f97880e8477a3d43f91cdfacd188e86cf71
Gustavo Ariel Coronel <ariel256@gmail.com>
2024-03-18 16:34:33 -0300

    Ended the project

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,51 @@
-<template>
-    <h1>Hola mundo en Vue3</h1>
+<template lang="pug">
+div 
+    h1 Hijo de div
+    h2 Hermano de H1
+    ol 
+        li.element1 manzana
+        li.element2 pera
+        li#element3 melón
+
+    a(
+        href="https://google.com" 
+        target="_blank"
+    ) Google con Pug
+
+    h2 Frutas
+    ol 
+        li(v-for="fruta in frutas") {{ fruta }}
+
+lista(:elmentos="juegos")
+
 </template>
 
 <script>
-export default {
+import Lista from "./components/Lista.vue"
 
+export default {
+    data: () => ({
+        frutas: ['manzana', 'pera', 'melón'],
+        juegos: ['CSGO', 'Minecraft', 'PUBG',],
+    }),
+    components: { Lista },
 }
 </script>
 
-<style>
+<style scoped>
+
+h1 {
+    color: red;
+}
+
+.element1 {
+    color:blue;
+}
+.element2 {
+    color: purple;
+}
+#element3 {
+    color: green;
+}
 
 </style>
